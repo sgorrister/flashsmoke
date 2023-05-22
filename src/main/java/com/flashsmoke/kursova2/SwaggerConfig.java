c6869766c94cc7c3204ca7fe45ef1b1bc77a81aa
sgorrister <olegdychko1@gmail.com>
2023-05-23 01:18:10 +0300

    Я пробував поремонтувати але воно далі нічого не працює

--- src/main/java/com/flashsmoke/kursova2/SwaggerConfig.java
+++ src/main/java/com/flashsmoke/kursova2/SwaggerConfig.java
@@ -1,61 +1,32 @@
+package com.flashsmoke.kursova2;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.*;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("your.package.name"))
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.flashsmoke.kursova2"))
                 .build()
-                .apiInfo(apiInfo())
-                .enable(true);
+                .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Your API Title")
-                .description("Your API Description")
+                .title("KursovaFlashsmokeAPI")
+                .description("Description")
                 .version("1.0.0")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi customOpenApi() {
-        String[] paths = {"/api/**"}; // Вкажіть шляхи до вашого API
-        return GroupedOpenApi.builder()
-                .group("custom-api")
-                .pathsToMatch(paths)
-                .build();
-    }
-
-    @Bean
-    public UiConfiguration uiConfig() {
-        return UiConfigurationBuilder.builder()
-                .deepLinking(true)
-                .displayOperationId(false)
-                .defaultModelsExpandDepth(1)
-                .defaultModelExpandDepth(1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .displayRequestDuration(false)
-                .docExpansion(DocExpansion.NONE)
-                .filter(false)
-                .maxDisplayedTags(null)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .showExtensions(false)
-                .tagsSorter(TagsSorter.ALPHA)
-                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-                .validatorUrl(null)
+                .contact(new Contact("asdasd", "your-website.com", "your-email@example.com"))
                 .build();
     }
 }
